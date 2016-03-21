ea251ff53e9c9222e772c89dea32d1cfd3300684
Redevelvet <redevelvet@RedevelvetdeMacBook-Pro.local>
2016-03-21 22:15:01 +0800

    ceshi 22222

--- org.redvelvet/redvelvet-example/redvelvet-example-web/src/main/java/redvelvet/example/Launcher.java
+++ org.redvelvet/redvelvet-example/redvelvet-example-web/src/main/java/redvelvet/example/Launcher.java
@@ -13,7 +13,7 @@ import org.redvelvet.webapp.jetty.JettyWebServer;
 public class Launcher {
 
 	public static void main(String[] args) {
-		//asdd
+		//asadsadasddssdd
 		JettyWebServer jettyWebServer=new JettyWebServer();
 		jettyWebServer.init().server();  
 	}
